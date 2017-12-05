8b473e0a3a4ae473be3a57d1a3a87ff697916dfd
Jon Samp <jon@codecademy.com>
2017-12-04 21:57:51 -0500

    initial commit

---
+++ copy_dotfiles.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "copying exports"
+cp ~/.exports ./
+
+echo "copying aliases"
+cp ~/.aliases ./
+
+echo "copying functions"
+cp ~/.functions ./
+
+echo "copying bash_prompt"
+cp ~/.bash_prompt ./
